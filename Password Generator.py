8732d359ef1dce6500f8d7c12685bc876c6b03a1
Arsheia Seddiqa Wahab <arsheiaseddiqa756@gmail.com>
2025-09-12 14:53:47 +0500

    Update Password Generator
    It is a very simple program which generates password of your desired length and how many passwords  you want.

--- Password Generator.py
+++ Password Generator.py
@@ -1,16 +1,38 @@
-import secrets
-"""I have used secrets instead of random because it gives truely
-unpredictable results than random. """
+#include <iostream>
+#include <string>
+#include <random>
 
-def password_generator():
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+using namespace std;
 
-    length = int(input("Enter your desired password length: "))
-    count = int(input("How many passwords do you want to generate: "))
-    for i in range(count):
-        password = ""
-        for j in range(length):
-            password += secrets.choice(characters)
-        print(f"password  {i+1}: {password}.")
-   
-password_generator()
+void password_generator() {
+    // character set (letters, digits, symbols)
+    string characters = "abcdefghijklmnopqrstuvwxyz"
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                        "0123456789"
+                        "!@#$%^&*()";
+
+    int length, count;
+    cout << "Enter your desired password length: ";
+    cin >> length;
+    cout << "How many passwords do you want to generate: ";
+    cin >> count;
+
+    // random device and generator (better than rand())
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<> dist(0, characters.size() - 1);
+
+    // generate passwords
+    for (int i = 0; i < count; i++) {
+        string password = "";
+        for (int j = 0; j < length; j++) {
+            password += characters[dist(generator)];
+        }
+        cout << "Password " << i + 1 << ": " << password << endl;
+    }
+}
+
+int main() {
+    password_generator();
+    return 0;
+}
